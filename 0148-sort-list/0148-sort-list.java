74bbd696bdc7c750746c830c81500eed5cb56499
SANGAM SUDHAKAR <sangemsudhakar8@gmail.com>
2026-08-21 12:13:36 +0530

    Time: 9 ms (97.56%), Space: 59.1 MB (94.69%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+    return head;
+}
+        ListNode slow = head;
+        ListNode fast=head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode right = slow.next;
+        slow.next =null;
+        ListNode left = sortList(head);
+        right = sortList(right);
+        return merge(left,right);   
+    }
+    private ListNode merge(ListNode left, ListNode right) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+
+        while (left != null && right != null) {
+
+            if (left.val <= right.val) {
+                temp.next = left;
+                left = left.next;
+            } else {
+                temp.next = right;
+                right = right.next;
+            }
+
+            temp = temp.next;
+        }
+        if (left != null) {
+            temp.next = left;
+        }
+
+        if (right != null) {
+            temp.next = right;
+        }
+
+        return dummy.next;
+    }
+}
