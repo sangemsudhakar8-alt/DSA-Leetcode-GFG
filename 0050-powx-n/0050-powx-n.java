f82d066ae746a070f881a80e9f2b54bc505c45d6
SANGAM SUDHAKAR <sangemsudhakar8@gmail.com>
2026-08-27 18:41:54 +0530

    Time: 0 ms (100%), Space: 48.2 MB (34.61%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1.0;
+        long nm= n;
+        if(nm<0){
+            nm = -1*nm;
+        }
+        while(nm>0){
+            if(nm%2 ==1){
+                ans = ans*x;
+                nm = nm-1;
+            }else{
+                x = x*x;
+                nm = nm/2;
+            }
+        }
+        if(n<0){
+            ans = (double)(1.0)/(double)(ans);
+        }
+    return ans;   
+    }
+}
