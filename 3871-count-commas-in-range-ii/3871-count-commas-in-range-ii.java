3a3b759f6628bb396b9a697327f91713823cb60b
SANGAM SUDHAKAR <sangemsudhakar8@gmail.com>
2026-09-09 18:09:23 +0530

    Time: 1 ms (99.36%), Space: 42.3 MB (96.79%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long countCommas(long n) {
+      long count = 0;
+        long base = 1000;
+        while(base<=n){
+            count +=(n-base+1);
+            base *=1000;
+        }
+        return count;
+    }
+}
